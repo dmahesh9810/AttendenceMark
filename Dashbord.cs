20568845732e1e00f2b4ea7dac4c5ba1a66fd824
unknown <dmahesh9810@gmail.com>
2024-07-10 13:11:18 +0530

    login form and database connecton

--- Dashbord.cs
+++ Dashbord.cs
@@ -6,25 +6,20 @@ public partial class Dashbord : Form
     {
         InitializeComponent();
 
-        this.Text = "My WinForms App";
-            this.Width = 800;
-            this.Height = 600;
+        this.Text = "Dashboard";
+        this.Width = 800;
+        this.Height = 600;
 
-            Button button1 = new Button();
-            button1.Text = "Click Me";
-            button1.Location = new System.Drawing.Point(350, 250);
-            button1.Click += (sender, e) => MessageBox.Show("Hello, World!");
 
-             Button button2 = new Button();
-            button2.Text = "Open Form2";
-            button2.Location = new System.Drawing.Point(350, 300);
-            button2.Click += (sender, e) =>
-            {
-                Login form2 = new Login();
-                form2.Show();
-            };
+        Button button1 = new Button();
+        button1.Text = "Open Form2";
+        button1.Location = new System.Drawing.Point(350, 300);
+        button1.Click += (sender, e) =>
+        {
+            Login form2 = new Login();
+            form2.Show();
+        };
 
-            this.Controls.Add(button1);
-            this.Controls.Add(button2);
+        this.Controls.Add(button1);
     }
 }
