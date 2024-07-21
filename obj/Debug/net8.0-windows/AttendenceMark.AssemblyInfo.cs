13dcd09a505fef780d9a7bfb26892f4903defb38
unknown <dmahesh9810@gmail.com>
2024-07-21 19:17:04 +0530

    Login fix

--- obj/Debug/net8.0-windows/AttendenceMark.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AttendenceMark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AttendenceMark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b4e3275f880f43a03d49fd2ba1485c113b7ac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb1f679e22c33926aee14472b2eb58be62337f40")]
 [assembly: System.Reflection.AssemblyProductAttribute("AttendenceMark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AttendenceMark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
